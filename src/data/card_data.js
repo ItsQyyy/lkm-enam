172a872fbb58928e06467057e375a0cf02838ce6
ItsQyyy <itsqyyy@gmail.com>
2026-05-13 22:33:10 +0700

    Card Component and Materi Page

---
+++ src/data/card_data.js
@@ -0,0 +1,22 @@
+export const cardData = [
+ {
+  id: 1,
+  title: "Belajar React.js dari Dasar hingga Mahir",
+  description:
+    "Kursus ini dirancang untuk membantu pemula memahami React.js secara bertahap mulai dari konsep dasar hingga pembuatan aplikasi modern yang interaktif. Materi dimulai dengan pengenalan React, cara kerja component, JSX, props, dan state. Setelah memahami dasar-dasarnya, peserta akan belajar bagaimana membuat tampilan dinamis menggunakan event handling, conditional rendering, serta rendering list data secara efisien.\n\nSelain itu, kursus ini juga membahas penggunaan hooks seperti useState dan useEffect yang menjadi inti pengembangan React modern. Peserta akan mempelajari cara mengambil data dari API, mengelola state aplikasi, serta membangun struktur project yang rapi dan mudah dikembangkan. Pada bagian lanjutan, materi akan mencakup React Router untuk navigasi halaman, reusable component, optimasi performa, dan praktik terbaik dalam pengembangan frontend.\n\nSetiap pembelajaran dilengkapi dengan studi kasus dan mini project agar peserta dapat langsung mempraktikkan materi yang dipelajari. Di akhir kursus, peserta akan mampu membuat aplikasi frontend modern seperti dashboard, website portfolio, sistem manajemen tugas, hingga aplikasi interaktif berbasis React dengan tampilan responsif dan clean UI.\n\nKursus ini cocok untuk siswa, mahasiswa, maupun developer pemula yang ingin memulai karier sebagai Frontend Developer modern. Tidak hanya teori, pembelajaran juga fokus pada praktik langsung sehingga peserta dapat memahami alur kerja React secara nyata dalam pengembangan aplikasi web masa kini.",
+  icon: "⚛️",
+  progress: 75,
+  duration: "20 Jam 30 Menit",
+  tag: "Frontend",
+  level: "Pemula",
+},
+  { id: 2, 
+    title: "Learn Next.js",
+    description: "A React framework for production.",
+    icon: "🚀",
+    progress: 30,
+    duration: "25 menit",
+    level: "Menengah",
+    href: "/learn-nextjs"
+  }
+];
